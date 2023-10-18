e9163871099dd0cc594812bab250ff0fc4c8095d
Jonas Schütz <jonasschutz@jonass-macbook-pro.home>
2023-10-18 22:54:25 +0200

    added material 3

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import '@material/web/button/filled-button.js';
+import '@material/web/button/outlined-button.js';
+import '@material/web/checkbox/checkbox.js';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <md-outlined-button>Back</md-outlined-button>
+    <md-filled-button>Next</md-filled-button>
   </main>
 </template>
